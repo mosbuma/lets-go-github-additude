72f827e57d62fccd29248517068ecff94763b449
Marc Buma <mosbuma@bumos.nl>
2020-05-01 16:19:47 +0200

    add index.html

---
+++ app/Team-Green-Rulez/src/main/java/com/additudeteamgreen/teamgreenrulez/TeamGreenRulezApplication.java
@@ -0,0 +1,13 @@
+package com.additudeteamgreen.teamgreenrulez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamGreenRulezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamGreenRulezApplication.class, args);
+	}
+
+}
---
+++ app/Team-Green-Rulez/src/test/java/com/additudeteamgreen/teamgreenrulez/TeamGreenRulezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.additudeteamgreen.teamgreenrulez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamGreenRulezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
